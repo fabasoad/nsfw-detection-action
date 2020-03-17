282b23c3da1871bcbfd5380dee1de12c2b58b246
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-17 22:02:19 +0900

    Trigger build

--- src/providers/sightengine.js
+++ src/providers/sightengine.js
@@ -7,14 +7,15 @@ const BASE_URL = 'https://api.sightengine.com/1.0/check.json';
 module.exports = (apiKey, file) => {
   const apiKeys = apiKey.split(',');
   const form = new FormData();
-  form.append('media', fs.createReadStream(file + "h"));
+  form.append('media', fs.createReadStream(file));
   form.append('api_user', apiKeys[0]);
   form.append('api_secret', apiKeys[1]);
-  form.append('models', 'nudity');
+  form.append('models', 'nudity1');
 
   return got
     .post(BASE_URL, { body: form })
     .then(({ body }) => {
+      console.log(body);
       const resp = JSON.parse(body);
       if (resp.status !== 'success') {
         const message = resp.error && resp.error.errorMsg
