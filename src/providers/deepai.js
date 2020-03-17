bda9d464eff5466831f9b57aba7178ff4b404924
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-17 16:43:52 +0900

    Fix bug with post request

--- src/providers/deepai.js
+++ src/providers/deepai.js
@@ -5,17 +5,13 @@ const fs = require('fs');
 const BASE_URL = 'https://api.deepai.org/api/nsfw-detector';
 
 module.exports = (apiKey, file) => {
-  const data = new FormData();
-  data.append('image', fs.createReadStream(file));
+  const form = new FormData();
+  form.append('image', fs.createReadStream(file));
 
-  return axios.post(
-    BASE_URL,
-    data,
-    {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        'api-key': apiKey
-      }
-    }
-  ).then((resp) => resp['output']['nsfw_score']);
+  const headers = form.getHeaders();
+  headers['api-key'] = apiKey;
+  return axios
+    .create({ headers: headers })
+    .post(BASE_URL, form)
+    .then((resp) => resp['output']['nsfw_score']);
 };
