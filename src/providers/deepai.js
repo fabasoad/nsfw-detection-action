b9159eb09b8ac87a823510be8e1c94014a4904c0
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-17 16:30:41 +0900

    Add axios dependency. Remove request dependency

--- src/providers/deepai.js
+++ src/providers/deepai.js
@@ -1,15 +1,18 @@
-const fs = require('fs');
-const request = require('request');
+const axios = require('axios');
 
 const BASE_URL = 'https://api.deepai.org/api/nsfw-detector';
 
-module.exports = (apiKey, file) => request({
-  method: 'POST',
-  uri: BASE_URL,
-  headers: {
-    'api-key': apiKey
-  },
-  formData: {
-    image: fs.createReadStream(file)
-  }
-});
+module.exports = (apiKey, file) => {
+  const data = new FormData();
+  data.append('image', file);
+
+  return axios.post(
+    BASE_URL,
+    data,
+    {
+      headers: {
+        'api-key': apiKey
+      }
+    }
+  ).then((resp) => resp['output']['nsfw_score']);
+};
