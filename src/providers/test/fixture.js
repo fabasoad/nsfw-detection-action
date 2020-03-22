fae0128b4b4fa9921dd103a0b414cd9d4ceb2a3a
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-22 13:33:53 +0900

    Format code

--- src/providers/test/fixture.js
+++ src/providers/test/fixture.js
@@ -1,31 +1,31 @@
 require('dotenv').config();
 
 module.exports = [{
-    provider: 'cloudmersive',
-    apiKey: process.env.CLOUDMERSIVE_API_KEY,
-    threshold: {
-        positive: 0.314,
-        negative: 0.315
-    }
+  provider: 'cloudmersive',
+  apiKey: process.env.CLOUDMERSIVE_API_KEY,
+  threshold: {
+    positive: 0.314,
+    negative: 0.315
+  }
 }, {
-    provider: 'deepai',
-    apiKey: process.env.DEEPAI_API_KEY,
-    threshold: {
-        positive: 0.395,
-        negative: 0.396
-    }
+  provider: 'deepai',
+  apiKey: process.env.DEEPAI_API_KEY,
+  threshold: {
+    positive: 0.395,
+    negative: 0.396
+  }
 }, {
-    provider: 'picpurify',
-    apiKey: process.env.PICPURIFY_API_KEY,
-    threshold: {
-        positive: 0.637,
-        negative: 0.638
-    }
+  provider: 'picpurify',
+  apiKey: process.env.PICPURIFY_API_KEY,
+  threshold: {
+    positive: 0.637,
+    negative: 0.638
+  }
 }, {
-    provider: 'sightengine',
-    apiKey: process.env.SIGHTENGINE_API_KEY,
-    threshold: {
-        positive: 0.448,
-        negative: 0.449
-    }
+  provider: 'sightengine',
+  apiKey: process.env.SIGHTENGINE_API_KEY,
+  threshold: {
+    positive: 0.448,
+    negative: 0.449
+  }
 }];
--- src/providers/test/test-providers.js
+++ src/providers/test/test-providers.js
@@ -9,27 +9,27 @@ const fixture = require('./fixture');
 const IMAGE_URL = 'https://images-na.ssl-images-amazon.com/images/I/91cDPlxcRiL._SL1500_.jpg';
 
 describe('Test NSFW detection', () => {
-    const pipeline = promisify(stream.pipeline);
-    const filePath = 'fiR75VM78Yviy3.jpg';
+  const pipeline = promisify(stream.pipeline);
+  const filePath = 'fiR75VM78Yviy3.jpg';
 
-    before(async () => {
-        await pipeline(
-            got.stream(IMAGE_URL),
-            fs.createWriteStream(filePath)
-        );
-    });
+  before(async () => {
+    await pipeline(
+      got.stream(IMAGE_URL),
+      fs.createWriteStream(filePath)
+    );
+  });
 
-    itParam('${value.provider} should detect NSFW content', fixture, async (arg) => {
-        const getScore = require(`../${arg.provider}`);
-        const score = await getScore(arg.apiKey, filePath);
-        assert(score >= arg.threshold.positive);
-    });
+  itParam('${value.provider} should detect NSFW content', fixture, async (arg) => {
+    const getScore = require(`../${arg.provider}`);
+    const score = await getScore(arg.apiKey, filePath);
+    assert(score >= arg.threshold.positive);
+  });
 
-    itParam('${value.provider} should not detect NSFW content', fixture, async (arg) => {
-        const getScore = require(`../${arg.provider}`);
-        const score = await getScore(arg.apiKey, filePath);
-        assert(score < arg.threshold.negative);
-    });
+  itParam('${value.provider} should not detect NSFW content', fixture, async (arg) => {
+    const getScore = require(`../${arg.provider}`);
+    const score = await getScore(arg.apiKey, filePath);
+    assert(score < arg.threshold.negative);
+  });
 
-    after(() => fs.unlinkSync(filePath));
+  after(() => fs.unlinkSync(filePath));
 });
