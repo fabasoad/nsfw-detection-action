3b2d436bff08961f11a7bfedf5543a3c4ca6cc7d
fabasoad <fabasoad@gmail.com>
2022-07-06 23:01:46 +0900

    Add Functional tests

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { getInput, setFailed, error, warning, info } from '@actions/core'
+import {
+  NsfwDetectionProviderFactory
+} from './translation/NsfwDetectionProviderFactory'
+import { GitHubUtils } from './utils/GitHubUtils'
+
+async function run() {
+  try {
+    const threshold = Number(getInput('threshold'));
+    const provider =
+        NsfwDetectionProviderFactory.getProvider(getInput('provider'))
+    const files: Set<string> = await GitHubUtils.getChangedFiles(
+      getInput('github_token'),
+      getInput('type').split(','),
+      getInput('extensions').split(',')
+    );
+
+    let count = 0
+    for (const file of files) {
+      const score: number = await provider.getScore(getInput('api_key'), file)
+      const result: number = threshold - score
+      const output = `Score: ${score}, File: ${file}`
+      if (result < 0) {
+        count++
+        error(`[ERROR  ] File is detected as NSFW. ${output}`)
+      } else if (result > 5) {
+        info(`[INFO   ] File is safe to be used. ${output}`)
+      } else {
+        warning(`[WARNING] File is closed to be detected as NSFW. ${output}`)
+      }
+    }
+    if (count > 0) {
+      setFailed(`\nThere are ${count} files have been detected as NSFW`)
+    }
+  } catch (e) {
+    setFailed((<Error>e).message);
+  }
+}
+
+run();
---
+++ src/translation/providers/DeepaiNsfwDetectionProvider.ts
@@ -0,0 +1,26 @@
+import FormData from 'form-data'
+import fs from 'fs'
+import NsfwDetectionProviderBase from './NsfwDetectionProviderBase';
+
+type DeepaiResponse = {
+  output: {
+    nsfw_score: number
+  }
+}
+
+export class DeepaiNsfwDetectionProvider extends NsfwDetectionProviderBase {
+  constructor() {
+    super('https://api.deepai.org/api/nsfw-detector');
+  }
+
+  public async getScore(apiKey: string, file: fs.PathLike): Promise<number> {
+    const body = new FormData();
+    body.append('image', fs.createReadStream(file));
+
+    const headers = body.getHeaders();
+    headers['api-key'] = apiKey;
+
+    const resp = await this.request<DeepaiResponse>(body, headers)
+    return resp.output.nsfw_score
+  }
+}
