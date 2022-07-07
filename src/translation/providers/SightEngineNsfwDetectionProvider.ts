2b677cf70ce3a13f98cab4cb891be8155fa2ba15
Yevhen Fabizhevskyi <yevhen.fabizhevskyi@woven-planet.global>
2022-07-07 20:27:50 +0900

    Test

--- src/translation/providers/SightEngineNsfwDetectionProvider.ts
+++ src/translation/providers/SightEngineNsfwDetectionProvider.ts
@@ -1,6 +1,8 @@
 import FormData from 'form-data'
 import fs from 'fs'
 import NsfwDetectionProviderBase from './NsfwDetectionProviderBase'
+import LoggerFactory from '../../utils/LoggerFactory'
+import { Logger } from 'winston'
 
 type SightEngineResponse = {
   status: string
@@ -11,12 +13,17 @@ type SightEngineResponse = {
 
 export class SightEngineNsfwDetectionProvider
   extends NsfwDetectionProviderBase {
+  private readonly logger: Logger =
+    LoggerFactory.create(SightEngineNsfwDetectionProvider.name)
+
   constructor() {
     super('https://api.sightengine.com/1.0/check.json')
   }
 
   public async getScore(apiKey: string, file: fs.PathLike): Promise<number> {
     const apiKeys = apiKey.split(',')
+    this.logger.debug(`API key has ${apiKeys.length} parts ` +
+      `[${apiKeys[0].length}:${apiKeys[1].length}]`)
     const body = new FormData()
     body.append('media', fs.createReadStream(file))
     body.append('api_user', apiKeys[0])
