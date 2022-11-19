782643b1e2c256cfa5845e5e64970cb3857ee9fe
fabasoad <fabasoad@gmail.com>
2022-11-20 00:25:03 +0900

    Debug 22

--- src/detection/providers/PicPurifyNsfwDetectionProvider.ts
+++ src/detection/providers/PicPurifyNsfwDetectionProvider.ts
@@ -17,7 +17,7 @@ export class PicPurifyNsfwDetectionProvider extends NsfwDetectionProviderBase {
 
   public async getScore(apiKey: string, file: fs.PathLike): Promise<number> {
     const body = new FormData()
-    console.log(apiKey)
+    console.log(file)
     body.append('file_image', fs.createReadStream(file))
     body.append('API_KEY', apiKey)
     body.append('task', 'porn_moderation,suggestive_nudity_moderation')
