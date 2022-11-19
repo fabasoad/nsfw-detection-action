15637e424f7cc0fd6e9abef0893427c7cc7922c0
fabasoad <fabasoad@gmail.com>
2022-11-20 00:01:38 +0900

    Debug 15

--- src/utils/GitHubClient.ts
+++ src/utils/GitHubClient.ts
@@ -1,8 +1,11 @@
 import { context, getOctokit } from '@actions/github'
-import { Commit } from '@octokit/webhooks-definitions/schema'
 import LoggerFactory from './LoggerFactory'
 import { Logger } from 'winston'
 import { WebhookPayload } from '@actions/github/lib/interfaces'
+import {
+  GetResponseTypeFromEndpointMethod,
+  GetResponseDataTypeFromEndpointMethod
+} from '@octokit/types'
 
 export class GitHubClient {
   private readonly logger: Logger = LoggerFactory.create(GitHubClient.name)
@@ -11,35 +14,44 @@ export class GitHubClient {
     gitHubToken: string,
     types: string[],
     extensions: string[]): Promise<Set<string>> {
+    const octokit = getOctokit(gitHubToken)
+
+    type CompareCommitsResponseType = GetResponseTypeFromEndpointMethod<
+      typeof octokit.rest.repos.compareCommits
+    >
+    type CompareCommitsResponseDataType = GetResponseDataTypeFromEndpointMethod<
+      typeof octokit.rest.repos.compareCommits
+    >
+
     const payload: WebhookPayload = context.payload
     const repo = context.repo.repo
     const owner = context.repo.owner
 
-    const octokit = getOctokit(gitHubToken)
-    const { data } = await octokit.rest.repos.compareCommits(
-      { owner, repo, base: payload.before, head: payload.after }
-    )
-    console.log('data.files:', data.files)
-    const count = 0;
+    const resp: CompareCommitsResponseType =
+      await octokit.rest.repos.compareCommits(
+        { owner, repo, base: payload.before, head: payload.after }
+      )
+    const data: CompareCommitsResponseDataType = resp.data
+    if (!data.files) {
+      throw new Error('Cannot retrieve files list')
+    }
+    const count = data.files.length;
     this.logger.info(`There ${count > 1 ? 'are' : 'is'} ${count}
       file${count > 1 ? 's' : ''} found between ${payload.before} &
       ${payload.after} commits`)
     const result = new Set<string>()
-    // for (const commit of commits) {
-    //   const count: number = resp.data.files.length
-    //   for (const file of resp.data.files) {
-    //     this.logger.debug(`File: ${file.filename}. Status: ${file.status}`)
-    //     if (types.includes(file.status)) {
-    //       const temp: string[] = file.filename.split('.')
-    //       if (extensions.map((e: string) => e.toLowerCase())
-    //         .includes(temp[temp.length - 1].toLowerCase())) {
-    //         result.add(file.filename)
-    //       }
-    //     }
-    //   }
-    //   this.logger.info(`There ${result.size > 1 ? 'are' : 'is'}` +
-    //     ` ${result.size} file${result.size > 1 ? 's' : ''} will be checked`)
-    // }
+    for (const file of data.files) {
+      this.logger.debug(`File: ${file.filename}. Status: ${file.status}`)
+      if (types.includes(file.status)) {
+        const temp: string[] = file.filename.split('.')
+        if (extensions.map((e: string) => e.toLowerCase())
+          .includes(temp[temp.length - 1].toLowerCase())) {
+          result.add(file.filename)
+        }
+      }
+    }
+    this.logger.info(`There ${result.size > 1 ? 'are' : 'is'}` +
+      ` ${result.size} file${result.size > 1 ? 's' : ''} will be checked`)
     return result
   }
 }
