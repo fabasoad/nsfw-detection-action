d386b40a9b57b688d8012f10a15ee4e92945a06e
fabasoad <fabasoad@gmail.com>
2025-01-03 15:01:42 +0900

    Debug 1

--- src/utils/HttpClient.ts
+++ src/utils/HttpClient.ts
@@ -2,7 +2,10 @@ export default class HttpClient {
   public async request<TResponse>(
     url: string, init?: RequestInit
   ): Promise<TResponse> {
+    console.log('>> 1')
     const { json } = await fetch(url, init)
+    console.log('>> 2')
+    console.dir(json())
     return await json() as TResponse
   }
 }
