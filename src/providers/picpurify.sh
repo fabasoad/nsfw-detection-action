03a23f373b84bf71796cf0561b8aaed95028e6a3
fabasoad <fabasoad@gmail.com>
2025-01-04 00:23:06 +0900

    Debug 6

--- src/providers/picpurify.sh
+++ src/providers/picpurify.sh
@@ -19,13 +19,14 @@ main() {
       -F "API_KEY=${api_key}" \
       -F "task=porn_moderation,suggestive_nudity_moderation" \
       -F "file_image=@${file_path}")
-    echo "${response}"
     if [ "$(echo "${response}" | jq -r '.status')" = "success" ]; then
       score=$(echo "${response}" | jq -r '.confidence_score_decision')
       log_info "Classified ${file_path} with score ${score}"
     else
-      msg="There was a problem during ${file_path} file classification. Code:"
-      msg="${msg} $(echo "${response}" | jq -r '.error | "Code: \(.errorCode). Reason: \(.errorMsg)"')"
+      msg="There was a problem during ${file_path} file classification."
+      if [ -n "${response}" ]; then
+        msg="${msg} $(echo "${response}" | jq -r '.error | "Code: \(.errorCode). Reason: \(.errorMsg)"')"
+      fi
       log_warning "${msg}"
     fi
   done
