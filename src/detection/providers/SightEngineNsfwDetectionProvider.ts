aed3993b3962c11b28077abd63114185765fabca
fabasoad <fabasoad@gmail.com>
2025-01-03 17:04:42 +0900

    Debug 10

--- src/detection/providers/SightEngineNsfwDetectionProvider.ts
+++ src/detection/providers/SightEngineNsfwDetectionProvider.ts
@@ -2,13 +2,22 @@ import FormData from 'form-data'
 import fs from 'fs'
 import NsfwDetectionProviderBase from './NsfwDetectionProviderBase'
 
+type SightEngineError = {
+  type: string,
+  code: number,
+  message: string
+}
+
+type SightEngineNudity = {
+  sexual_activity: number,
+  sexual_display: number,
+  erotica: number
+}
+
 type SightEngineResponse = {
-  status: string
-  nudity: {
-    sexual_activity: number,
-    sexual_display: number,
-    erotica: number,
-  }
+  status: string,
+  nudity?: SightEngineNudity,
+  error?: SightEngineError
 }
 
 export class SightEngineNsfwDetectionProvider
@@ -21,14 +30,21 @@ export class SightEngineNsfwDetectionProvider
     const apiKeys: string[] = apiKey.split(',')
     const body = new FormData()
     body.append('media', fs.createReadStream(file))
+    body.append('models', 'nudity-2.1')
     body.append('api_user', apiKeys[0])
     body.append('api_secret', apiKeys[1])
-    body.append('models', 'nudity-2.1')
 
-    const { status, nudity } = await this.request<SightEngineResponse>(body)
-    if (status !== 'success') {
-      this.logger.warning(`There was a problem during ${file} file classification`)
+    const { status, nudity, error } = await this.request<SightEngineResponse>(body)
+    if (status === 'failure') {
+      const { type, code, message }: SightEngineError = error!
+      this.logger.warning(
+        `There was a problem during ${file} file classification. Type: ${type}.`
+        + ` Code: ${code}. Reason: ${message}`
+      )
+      return 0
+    } else {
+      const { sexual_activity, sexual_display, erotica } = nudity!
+      return Math.max(sexual_activity, sexual_display, erotica)
     }
-    return Math.max(nudity.sexual_activity, nudity.sexual_display, nudity.erotica)
   }
 }
