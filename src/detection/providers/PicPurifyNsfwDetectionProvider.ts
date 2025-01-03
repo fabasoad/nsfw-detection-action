314b3d95416e7bcd817bc5a830ef05acf9d196d8
fabasoad <fabasoad@gmail.com>
2025-01-03 17:25:55 +0900

    Debug 13

--- src/detection/providers/PicPurifyNsfwDetectionProvider.ts
+++ src/detection/providers/PicPurifyNsfwDetectionProvider.ts
@@ -1,14 +1,17 @@
 import FormData from 'form-data'
-import { PathLike, Stats, statSync, createReadStream } from 'fs'
-import { resolve } from 'path'
+import { PathLike, statSync, createReadStream } from 'fs'
+import { basename } from 'path'
 import NsfwDetectionProviderBase from './NsfwDetectionProviderBase'
+
+type PicPurifyError = {
+  errorCode: number,
+  errorMsg: string
+}
 
 type PicPurifyResponse = {
   status: string
-  error: {
-    errorMsg: string
-  }
-  confidence_score_decision: number
+  error?: PicPurifyError
+  confidence_score_decision?: number
 }
 
 export class PicPurifyNsfwDetectionProvider extends NsfwDetectionProviderBase {
@@ -18,21 +21,25 @@ export class PicPurifyNsfwDetectionProvider extends NsfwDetectionProviderBase {
 
   public async getScore(apiKey: string, file: PathLike): Promise<number> {
     const body = new FormData()
-    const { size }: Stats = statSync(file)
     body.append('file_image', createReadStream(file), {
-      knownLength: size,
-      filename: file.toString(),
-      filepath: resolve(file.toString())
+      knownLength: statSync(file).size,
+      filename: basename(file.toString()),
+      filepath: file.toString()
     })
     body.append('API_KEY', apiKey)
     body.append('task', 'porn_moderation,suggestive_nudity_moderation')
 
-    const resp = await this.request<PicPurifyResponse>(body)
-    if (resp.status !== 'success') {
-      const message = resp.error && resp.error.errorMsg ?
-        resp.error.errorMsg : `Failed to analyze ${file}.`
-      throw new Error(message)
+    const { status, error, confidence_score_decision } =
+      await this.request<PicPurifyResponse>(body)
+    if (status === 'failure') {
+      const { errorCode, errorMsg }: PicPurifyError = error!
+      this.logger.warning(
+        `There was a problem during ${file} file classification. `
+          + `Code: ${errorCode}. Reason: ${errorMsg}`
+      )
+      return 0
+    } else {
+      return confidence_score_decision!
     }
-    return resp.confidence_score_decision
   }
 }
