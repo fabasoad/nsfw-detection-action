c0d836a4140ce1271f5a88b7819292cc27f0423d
fabasoad <fabasoad@gmail.com>
2025-01-04 01:32:20 +0900

    Debug 14

--- src/compare-with-threshold.sh
+++ src/compare-with-threshold.sh
@@ -6,10 +6,10 @@ compare_with_threshold() {
   file_path="${3}"
 
   result=$((threshold - score))
-  if awk -v r="${result}" 'BEGIN {exit (r < 0)}'; then
+  if awk -v r="${result}" 'BEGIN {exit !(r < 0)}'; then
     log_error "NSFW content detected in ${file_path} with score ${score}."
     exit 1
-  elif awk -v r="${result}" 'BEGIN {exit (r > 0.2)}'; then
+  elif awk -v r="${result}" 'BEGIN {exit !(r > 0.3)}'; then
     log_info "No NSFW content detected in ${file_path} with score ${score}."
   else
     log_warning "${file_path} with score ${score} is close to be considered as NSFW content."
