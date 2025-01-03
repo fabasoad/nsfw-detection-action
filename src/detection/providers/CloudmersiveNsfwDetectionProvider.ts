ff6740226c179636f2d7232c454a1140fd5de337
fabasoad <fabasoad@gmail.com>
2025-01-03 15:45:30 +0900

    Debug 8

--- src/detection/providers/CloudmersiveNsfwDetectionProvider.ts
+++ src/detection/providers/CloudmersiveNsfwDetectionProvider.ts
@@ -2,10 +2,6 @@ import fs from 'fs'
 import CloudmersiveImageApiClient from 'cloudmersive-image-api-client'
 import NsfwDetectionProviderBase from './NsfwDetectionProviderBase'
 
-type CloudmersiveResponse = {
-  Score: number
-}
-
 export class CloudmersiveNsfwDetectionProvider
   extends NsfwDetectionProviderBase {
   constructor() {
@@ -20,26 +16,12 @@ export class CloudmersiveNsfwDetectionProvider
     const imageFile = Buffer.from(fs.readFileSync(file).buffer)
     return new Promise((resolve, reject) => {
       apiInstance.nsfwClassify(imageFile, (error, data, response) => {
-        console.dir(error)
-        console.dir(data)
-        console.dir(response)
         if (error) {
           reject(error)
         } else {
-          resolve(data)
+          resolve(Number(data['Score']))
         }
       })
     })
-    // const client = CloudmersiveValidateApiClient.ApiClient.instance
-    // console.dir(client)
-    // return 0
-    // const body = new FormData()
-    // body.append('imageFile', fs.createReadStream(file))
-    //
-    // const headers = body.getHeaders()
-    // headers['apikey'] = apiKey
-    //
-    // const resp = await this.request<CloudmersiveResponse>(body, headers)
-    // return resp.Score
   }
 }
