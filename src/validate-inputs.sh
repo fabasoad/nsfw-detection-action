0c26b84eff6e092b76de1b2f975641cfe3abeb15
fabasoad <fabasoad@gmail.com>
2025-01-04 14:40:44 +0900

    Fix validation error

--- src/validate-inputs.sh
+++ src/validate-inputs.sh
@@ -66,7 +66,7 @@ main() {
   check_is_not_empty "api-key" "${input_api_key}"
   check_number "threshold" "${input_threshold}"
   check_is_not_empty "extensions" "${input_extensions}"
-  check_is_not_empty "types" "${input_type}"
+  check_is_not_empty "types" "${input_types}"
   echo "${input_types}" | while IFS=',' read -r input_type; do
     check_enum "types" "${input_type}" "added,copied,modified,renamed"
   done
